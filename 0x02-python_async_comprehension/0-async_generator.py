176334e696acaff90a3618f92e2a202d95e8adc8
onokajames96 <jamesonoka996@gmail.com>
2024-03-22 14:38:47 +0000

    asyncronous generator

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+Aysncronous Generator function
+"""
+import asyncio
+import random
+from typing import Generator
+
+
+async def async_generator():
+    """
+    Coroutine that asynchronously generates random numbers.
+    """
+    for _ in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
