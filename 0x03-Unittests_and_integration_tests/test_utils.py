06b97a4f5f15742c9b76b0bb7d6da7435544b456
onokajames96 <jamesonoka996@gmail.com>
2024-04-08 08:15:24 +0000

    task 2

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -17,3 +17,16 @@ class TestAccessNestedMap(unittest.TestCase):
     def test_access_nested_map(self, nested_map, path, expected_output):
         """method returns what it is supposed to."""
         self.assertEqual(access_nested_map(nested_map, path), expected_output)
+
+    @parameterized.expand([
+        ({}, ("a,"), "a")
+        ({"a": 1}, ("a", "b"), "b")
+    ])
+    def test_access_nested_map_exception(self, nested_map, path, message):
+        """
+        Test case to verify that KeyError is raised with the expected message.
+        """
+        with self.assertRaises(KeyError) as e:
+            access_nested_map(nested_map, path)
+
+        self.assertEqual(str(e.exception), message)
