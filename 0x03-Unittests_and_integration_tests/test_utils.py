1b161cad4248be766433efd6d649e3024d4fbc27
onokajames96 <jamesonoka996@gmail.com>
2024-04-08 08:17:50 +0000

    task 2

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -22,11 +22,11 @@ class TestAccessNestedMap(unittest.TestCase):
         ({}, ("a,"), "a")
         ({"a": 1}, ("a", "b"), "b")
     ])
-    def test_access_nested_map_exception(self, nested_map, path, message):
+    def test_access_nested_map_exception(self, nested_map, path, expected):
         """
         Test case to verify that KeyError is raised with the expected message.
         """
         with self.assertRaises(KeyError) as e:
             access_nested_map(nested_map, path)
 
-        self.assertEqual(str(e.exception), message)
+        self.assertEqual(str(e.exception), expected)
